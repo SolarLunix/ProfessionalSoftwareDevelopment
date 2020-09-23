fd9324cadc13d3499f197148c63e83da8ad8c2bb
Melissa <solarlunix@gmail.com>
2020-09-23 13:53:11 +0100

    Assignment 1
    Created the Me1 class and did what was asked via the lab

---
+++ Display/src/Week1/Me1.java
@@ -0,0 +1,32 @@
+/*
+ *   ProfessionalSoftwareDevelopment:Week1
+ *   File:          Me1
+ *   Created by:    Melissa Melaugh
+ *   Created on:    23/09/2020
+ *   Updated on:    23/09/2020
+ *   Project Description: A simple program to display information about a person named Melissa Melaugh
+ */
+package Week1;
+
+public class Me1
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+        System.out.print("Name: \tMelissa Melaugh");
+        System.out.print("\t\t\t");
+        System.out.println("Address: \t1 Cloudfoot Lane");
+        System.out.print("\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println("Rainbow, Co. Antrim, BT1 IDC");
+        System.out.print("\t\t\t\t\t\t\t\t\t\t\t");
+        System.out.println("Ireland");
+        System.out.println();
+        System.out.println("Phone: \t0123456789");
+        System.out.println();
+        System.out.println("eMail: \tsolarlunix@fakeemail.com");
+        System.out.println();
+        System.out.print("Height: \t5'6\"");
+        System.out.print("\t\t\t\t");
+        System.out.println("Weight: \t1 feather");
+    }
+}
