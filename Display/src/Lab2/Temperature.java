eb7daba75dc3fa2d10ae7b0f9db701e07e95f446
Melissa <solarlunix@gmail.com>
2020-09-25 12:14:59 +0100

    Created a temperature converter

---
+++ Display/src/Lab2/Temperature.java
@@ -0,0 +1,50 @@
+/*
+ *   ProfessionalSoftwareDevelopment:Lab2
+ *   File: //TODO
+ *   Created by: Melissa Melaugh
+ *   Created on: 25/09/2020
+ *   Updated on: 25/09/2020
+ *   Project Description: //TODO
+ */
+package Lab2;
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Temperature {
+    static private double RATIO = (9.0/5);
+    static private Scanner sc = new Scanner(System.in);
+
+    static public void main(String[] args){
+        double c=0, f=0;
+        char type;
+        DecimalFormat df = new DecimalFormat("0.0");
+
+        System.out.println("What is your input type? Type C for Celcius and F for Farienheight then press enter");
+        type = sc.next().charAt(0);
+        type = Character.toLowerCase(type);
+        System.out.println("What is the temperature you want converted? (Press enter when done)");
+
+        if (type == 'c'){
+            c = sc.nextDouble();
+            f = celciusToFarenheight(c);
+        } else if (type == 'f') {
+            f = sc.nextDouble();
+            c = farenheightToCelcius(f);
+        } else {
+            System.out.println("Unsupported temperature!");
+            System.exit(1);
+        }
+        System.out.println("Celsius: " + df.format(c));
+        System.out.println("Farenheight: " + df.format(f));
+    }
+
+    static private double celciusToFarenheight(double c){
+        return (c * RATIO) + 32;
+    }
+
+    static private double farenheightToCelcius(double f){
+        return ((f - 32) / RATIO);
+    }
+}
