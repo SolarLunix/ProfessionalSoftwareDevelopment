a5e4bfe7b1fb4a165e1afc9e56c5dc52b1d7901d
Melissa <solarlunix@gmail.com>
2020-10-14 11:08:46 +0100

    Update DaysInMonth1.java

--- Display/src/Lab5/DaysInMonth1.java
+++ Display/src/Lab5/DaysInMonth1.java
@@ -41,6 +41,22 @@ public class DaysInMonth1 {
             default:
                 days = 31;
         }
+        
+        /*
+        //OR It could be like this:
+        if (month == 2){
+            leapYear = isLeapYear(year);
+            if(leapYear){
+                days = 29;
+            } else {
+                days = 28;
+            }
+        } else if (month == 4  || month == 6 || month == 9 || month == 11){
+            days = 30;
+        } else {
+            days = 31;
+        }
+           */
 
         System.out.println("In " + year + ", month " + month + " there were " + days + " days.");
     }
