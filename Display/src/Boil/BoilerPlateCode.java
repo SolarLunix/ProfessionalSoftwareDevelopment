2129088abe84d18a029a8af14f4bc85144141231
Melissa <solarlunix@gmail.com>
2020-10-22 13:30:40 +0100

    Create BoilerPlateCode.java

---
+++ Display/src/Boil/BoilerPlateCode.java
@@ -0,0 +1,35 @@
+package Boil;
+
+import java.util.Scanner;
+
+/*******
+ *   ProfessionalSoftwareDevelopment:Boil
+ *   File: BoilerPlateCode
+ *   Created by: SolarLunix
+ *   Created on: 21/10/2020
+ *   Updated on: 21/10/2020
+ *   Project Description: This is boilerplate code for things that I need done quickly.
+ *******/
+public class BoilerPlateCode {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int getNextInt(String reqest){
+        int nextint = 0;
+        System.out.println(reqest);
+        nextint = in.nextInt();
+        in.nextLine();
+        return nextint;
+    }
+
+    private static double getNextDouble(String reqest){
+        double nextdouble = 0;
+        System.out.println(reqest);
+        nextdouble = in.nextDouble();
+        in.nextLine();
+        return nextdouble;
+    }
+}
