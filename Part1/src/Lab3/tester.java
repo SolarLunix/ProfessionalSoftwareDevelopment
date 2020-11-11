06f2fe0ae0a57b7da931589d545393f077e8dc91
Melissa <solarlunix@gmail.com>
2020-11-11 13:21:29 +0000

    End Class 1 - Moved everything from the first class into its own folder

---
+++ Part1/src/Lab3/tester.java
@@ -0,0 +1,15 @@
+/*
+ *   ProfessionalSoftwareDevelopment:Lab3
+ *   File: tester
+ *   Created by: Melissa Melaugh
+ *   Created on: 01/10/2020
+ *   Updated on: 01/10/2020
+ *   Project Description: //TODO
+ */
+package Lab3;
+
+public class tester {
+    public static void main(String[] args) {
+
+    }
+}//end tester
