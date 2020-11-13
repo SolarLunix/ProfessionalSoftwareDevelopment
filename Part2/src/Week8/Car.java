4bcd7e714bd979e02869b62b96279b8757c9a16b
Melissa <solarlunix@gmail.com>
2020-11-13 11:50:35 +0000

    Wednesday Class

---
+++ Part2/src/Week8/Car.java
@@ -0,0 +1,145 @@
+package Week8;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+
+/*******
+ *   Part2:Week8
+ *   File: Car
+ *   Created by: Melissa Melaugh
+ *   Created on: 11/11/2020
+ *   Updated on: 11/11/2020
+ *   Project Description: To store information about a Car
+ *******/
+public class Car {
+    private static int noOfCars = 0;
+    private int id;
+    private String make;
+    private String ownerName;
+    private String colour;
+    private int yearOfManufacture;
+    private int topSpeed;
+    private double costPrice;
+
+    Car(int id, String make, String ownerName, String colour, int yearOfManufacture, int topSpeed, double costPrice){
+        noOfCars++;
+        this.id = id;
+        this.make = make;
+        this.ownerName = ownerName;
+        this.colour = colour;
+        this.yearOfManufacture = yearOfManufacture;
+        this.topSpeed = topSpeed;
+        this.costPrice = costPrice;
+    }
+
+    Car(int id, String make, String ownerName, String colour){
+        noOfCars++;
+        this.id = id;
+        this.make = make;
+        this.ownerName = ownerName;
+        this.colour = colour;
+    }
+
+    Car(int id, String make, int topSpeed){
+        noOfCars++;
+        this.id = id;
+        this.make = make;
+        this.topSpeed = topSpeed;
+    }
+
+    Car(){
+        noOfCars++;
+    }
+
+    public void printCarInfo(){
+        DecimalFormat df = new DecimalFormat("0,000.00");
+        System.out.println("***********************************");
+        System.out.println("ID - " + this.id);
+        System.out.println("Make - " + this.make);
+        System.out.println("Owner - " + this.ownerName);
+        System.out.println("Colour - " + this.colour);
+        System.out.println("Year - " + this.yearOfManufacture);
+        System.out.println("Speed - " + this.topSpeed);
+        System.out.println("Cost - £" + df.format(this.costPrice));
+        System.out.println("***********************************");
+    }
+
+    public int ageOfCar(){
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int age = -1;
+
+        if(this.yearOfManufacture > 1900){
+            age = year - this.yearOfManufacture;
+        }
+
+        return age;
+    }
+
+    public double currentValue(int depricationRate){
+        if (this.yearOfManufacture == 0 || this.costPrice == 0){
+            return -1;
+        }
+        double currentValue = this.costPrice - (this.costPrice * (depricationRate/100) * this.ageOfCar());
+        return currentValue;
+    }
+
+    public void setID(int itsID) {
+        this.id = itsID;
+    }
+
+    public void setMake(String itsMake) {
+        this.make = itsMake;
+    }
+
+    public void setOwner(String whoOwns) {
+        this.ownerName = whoOwns;
+    }
+
+    public void setColour(String itsColour) {
+        this.colour = itsColour;
+    }
+
+    public void setYear(int whenBuild) {
+        this.yearOfManufacture = whenBuild;
+    }
+
+    public void setSpeed(int maxSpeed) {
+        this.topSpeed = maxSpeed;
+    }
+
+    public void setCost(double price) {
+        this.costPrice = price;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getOwner() {
+        return ownerName;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getYear() {
+        return yearOfManufacture;
+    }
+
+    public int getSpeed() {
+        return topSpeed;
+    }
+
+    public double getCost() {
+        return costPrice;
+    }
+
+    public int getNoOfCars(){
+        return noOfCars;
+    }
+}
---
+++ Part2/src/Week8/TestCar.java
@@ -0,0 +1,49 @@
+package Week8;
+
+/*******
+ *   Part2:Week8
+ *   File: TestCar
+ *   Created by: Melissa Melaugh
+ *   Created on: 11/11/2020
+ *   Updated on: 11/11/2020
+ *   Project Description: A testbed for the Car class
+ *******/
+public class TestCar {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Car sistersCar = new Car(100, "Ford", "My Sister Mary", "Red", 2008, 105, 10995);
+        Car bossCar = new Car(101, "Honda", "Pat Bell", "Silver", 2011, 210, 27500);
+        Car aCar = new Car(102, "Ford", "Janet Allison", "Red");
+        Car bCar = new Car(103, "John Smith", 100);
+
+        myCar.printCarInfo();
+        sistersCar.printCarInfo();
+        bossCar.printCarInfo();
+        aCar.printCarInfo();
+        bCar.printCarInfo();
+
+        bCar.setMake("Vauxhall");
+        bCar.setOwner("James Rodgers");
+        bCar.setYear(2012);
+        bCar.setCost(10000);
+
+        bCar.printCarInfo();
+
+        Car cCar = new Car();
+        cCar.setID(104);
+        cCar.setMake("Toyota");
+        cCar.setOwner("Robert Smith");
+        cCar.setColour("Blue");
+        cCar.setYear(2010);
+        cCar.setSpeed(120);
+        cCar.setCost(12000);
+
+        cCar.printCarInfo();
+
+        System.out.println("cCar age: " + cCar.ageOfCar());
+        System.out.println("myCar age: " + myCar.ageOfCar());
+        System.out.println("cCar current value: " + cCar.currentValue(10));
+        System.out.println("myCar current value: " + myCar.currentValue(10));
+        System.out.println("Number of cars: " + myCar.getNoOfCars());
+    }
+}
